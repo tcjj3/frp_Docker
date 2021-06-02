421aabb0228ad32e50f4df9d50f4c3888a6958ff
Chaojun Tan <495825947@qq.com>
2021-06-02 16:09:03 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,69 @@
+FROM debian:buster-slim
+
+LABEL maintainer "Chaojun Tan <https://github.com/tcjj3>"
+
+ADD entrypoint.sh /opt/entrypoint.sh
+
+RUN export DIR_TMP="$(mktemp -d)" \
+  && cd $DIR_TMP \
+  && chmod +x /opt/*.sh \
+  && sed -i "s/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
+  && sed -i "s/security.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
+  && apt-get update \
+  || echo "continue..." \
+  && apt-get install --no-install-recommends -y wget \
+                                                ca-certificates \
+                                                curl \
+                                                unzip \
+                                                procps \
+                                                psmisc \
+                                                net-tools \
+                                                cron \
+  && if [ "$(dpkg --print-architecture)" = "armhf" ]; then \
+        ARCH="arm"; \
+     else \
+        ARCH=$(dpkg --print-architecture); \
+     fi \
+  && cd ${DIR_TMP} \
+  && curl -L -o ${DIR_TMP}/frp_linux.tar.gz https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_${ARCH}.tar.gz \
+  && tar zxf /tmp/frp_linux.tar.gz -C ${DIR_TMP} \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frpc /usr/local/bin/frpc \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frps /usr/local/bin/frps \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frpc.ini /usr/local/bin/frpc.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frps.ini /usr/local/bin/frps.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frpc_full.ini /usr/local/bin/frpc_full.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frps_full.ini /usr/local/bin/frps_full.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frpc.ini /frpc.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frps.ini /frps.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frpc_full.ini /frpc_full.ini \
+  && cp ${DIR_TMP}/frp_0.36.2_linux_amd64/frps_full.ini /frps_full.ini \
+  && curl -L -o ${DIR_TMP}/frp.freefrp.net.zip https://freefrp.net/down/frp.freefrp.net.zip \
+  && unzip -o ${DIR_TMP}/frp.freefrp.net.zip -d ${DIR_TMP} \
+  && cp ${DIR_TMP}/frp.freefrp.net/frp.freefrp.net.cer /usr/local/bin/frp.freefrp.net.cer \
+  && cp ${DIR_TMP}/frp.freefrp.net/frp.freefrp.net.key /usr/local/bin/frp.freefrp.net.key \
+  && cp ${DIR_TMP}/frp.freefrp.net/frp.freefrp.net.cer /frp.freefrp.net.cer \
+  && cp ${DIR_TMP}/frp.freefrp.net/frp.freefrp.net.key /frp.freefrp.net.key \
+  && rm -rf ${DIR_TMP}
+
+
+
+
+
+
+
+
+
+
+
+ENTRYPOINT ["sh", "-c", "/opt/entrypoint.sh"]
+
+
+
+
+
+
+
+
+
+
+
---
+++ entrypoint.sh
@@ -0,0 +1,233 @@
+#!/bin/sh
+
+
+
+
+Frpc_Conf_Path="/usr/local/bin/frpc_custom.ini"
+
+Domain_Infos="freefrp_net"
+
+
+
+
+
+
+
+
+if [ ! -z "$SERVER_ADDR" ]; then
+if [ ! -z "$SERVER_PORT" ]; then
+if [ ! -z "$TOKEN" ]; then
+
+
+
+
+if [ ! -z "$PROTOCOL" ]; then
+
+if [ ! -z "$LOCAL_IP" ]; then
+if [ ! -z "$LOCAL_PORT" ]; then
+
+if [ ! -z "$SUBDOMAIN" ] || [ ! -z "$CUSTOM_DOMAINS" ]; then
+
+
+
+
+
+
+echo "" > ${Frpc_Conf_Path}
+
+echo "[common]" >> ${Frpc_Conf_Path}
+echo "server_addr = ${SERVER_ADDR}" >> ${Frpc_Conf_Path}
+echo "server_port = ${SERVER_PORT}" >> ${Frpc_Conf_Path}
+echo "token = ${TOKEN}" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+
+echo "admin_addr = 0.0.0.0" >> ${Frpc_Conf_Path}
+echo "admin_port = 7400" >> ${Frpc_Conf_Path}
+echo "admin_user = admin" >> ${Frpc_Conf_Path}
+echo "admin_pwd = admin" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+
+echo "login_fail_exit = false" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+
+echo "tls_enable = true" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+
+
+
+
+tmp_CUSTOM_DOMAINS=`echo "${CUSTOM_DOMAINS}" | sed "s/\./_/gi"`
+
+
+if [ "$PROTOCOL" -eq "https" ]; then
+echo "[${SUBDOMAIN}_${Domain_Infos}_${tmp_CUSTOM_DOMAINS}_https]" >> ${Frpc_Conf_Path}
+echo "type = https" >> ${Frpc_Conf_Path}
+echo "local_ip = ${LOCAL_IP}" >> ${Frpc_Conf_Path}
+echo "local_port = ${LOCAL_PORT}" >> ${Frpc_Conf_Path}
+[ ! -z "$CUSTOM_DOMAINS" ] && echo "custom_domains = ${CUSTOM_DOMAINS}" >> ${Frpc_Conf_Path}
+[ ! -z "$SUBDOMAIN" ] && echo "subdomain = ${SUBDOMAIN}" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+fi
+
+
+if [ "$PROTOCOL" -eq "http" ] || [ "$PROTOCOL" -eq "httponly" ]; then
+echo "[${SUBDOMAIN}_${Domain_Infos}_${tmp_CUSTOM_DOMAINS}_http]" >> ${Frpc_Conf_Path}
+echo "type = http" >> ${Frpc_Conf_Path}
+echo "local_ip = ${LOCAL_IP}" >> ${Frpc_Conf_Path}
+echo "local_port = ${LOCAL_PORT}" >> ${Frpc_Conf_Path}
+[ ! -z "$CUSTOM_DOMAINS" ] && echo "custom_domains = ${CUSTOM_DOMAINS}" >> ${Frpc_Conf_Path}
+[ ! -z "$SUBDOMAIN" ] && echo "subdomain = ${SUBDOMAIN}" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+fi
+
+
+if [ "$PROTOCOL" -eq "https2http" ] || [ "$PROTOCOL" -eq "http" ]; then
+echo "[${SUBDOMAIN}_${Domain_Infos}_${tmp_CUSTOM_DOMAINS}_https2http]" >> ${Frpc_Conf_Path}
+echo "type = https" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+echo "plugin = https2http" >> ${Frpc_Conf_Path}
+echo "plugin_local_addr = ${LOCAL_IP}:${LOCAL_PORT}" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+[ ! -z "$CUSTOM_DOMAINS" ] && echo "custom_domains = ${CUSTOM_DOMAINS}" >> ${Frpc_Conf_Path}
+[ ! -z "$SUBDOMAIN" ] && echo "subdomain = ${SUBDOMAIN}" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+
+echo "plugin_crt_path = /usr/local/bin/frp.freefrp.net.cer" >> ${Frpc_Conf_Path}
+echo "plugin_key_path = /usr/local/bin/frp.freefrp.net.key" >> ${Frpc_Conf_Path}
+
+[ ! -z "$CUSTOM_DOMAINS" ] && echo "plugin_host_header_rewrite = ${CUSTOM_DOMAINS}" >> ${Frpc_Conf_Path}
+[ ! -z "$SUBDOMAIN" ] && echo "plugin_host_header_rewrite = ${SUBDOMAIN}.freefrp.net" >> ${Frpc_Conf_Path}
+
+echo "plugin_header_X-From-Where = frp" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+fi
+
+
+if [ "$PROTOCOL" -eq "tcp" ] || [ "$PROTOCOL" -eq "udp" ]; then
+echo "[${Domain_Infos}_${REMOTE_PORT}_${PROTOCOL}]" >> ${Frpc_Conf_Path}
+echo "type = ${PROTOCOL}" >> ${Frpc_Conf_Path}
+echo "local_ip = ${LOCAL_IP}" >> ${Frpc_Conf_Path}
+echo "local_port = ${LOCAL_PORT}" >> ${Frpc_Conf_Path}
+
+if [ ! -z "$REMOTE_PORT" ]; then
+echo "remote_port = ${REMOTE_PORT}" >> ${Frpc_Conf_Path}
+else
+	echo >&2 'error: missing required REMOTE_PORT environment variable'
+	echo >&2 '  Did you forget to -e REMOTE_PORT=... ?'
+	exit 1
+fi
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+fi
+
+
+if [ "$PROTOCOL" -eq "unix_domain_socket" ]; then
+echo "[${Domain_Infos}_${REMOTE_PORT}_${PROTOCOL}]" >> ${Frpc_Conf_Path}
+echo "type = ${PROTOCOL}" >> ${Frpc_Conf_Path}
+
+if [ ! -z "$REMOTE_PORT" ]; then
+echo "remote_port = ${REMOTE_PORT}" >> ${Frpc_Conf_Path}
+else
+	echo >&2 'error: missing required REMOTE_PORT environment variable'
+	echo >&2 '  Did you forget to -e REMOTE_PORT=... ?'
+	exit 1
+fi
+
+echo "plugin = unix_domain_socket" >> ${Frpc_Conf_Path}
+
+if [ ! -z "$UNIX_PATH" ]; then
+echo "plugin_unix_path = ${UNIX_PATH}" >> ${Frpc_Conf_Path}
+else
+	echo >&2 'error: missing required UNIX_PATH environment variable'
+	echo >&2 '  Did you forget to -e UNIX_PATH=... ?'
+	exit 1
+fi
+echo "" >> ${Frpc_Conf_Path}
+echo "" >> ${Frpc_Conf_Path}
+fi
+
+
+
+
+
+
+
+
+frpc -c ${Frpc_Conf_Path} > /dev/null 2>&1
+
+
+
+
+
+
+
+
+
+
+else
+	echo >&2 'error: missing required SUBDOMAIN or CUSTOM_DOMAINS environment variable'
+	echo >&2 '  Did you forget to -e SUBDOMAIN=... or -e CUSTOM_DOMAINS=... ?'
+	exit 1
+fi
+
+else
+	echo >&2 'error: missing required LOCAL_PORT environment variable'
+	echo >&2 '  Did you forget to -e LOCAL_PORT=... ?'
+	exit 1
+fi
+else
+	echo >&2 'error: missing required LOCAL_IP environment variable'
+	echo >&2 '  Did you forget to -e LOCAL_IP=... ?'
+	exit 1
+fi
+
+else
+	echo >&2 'error: missing required PROTOCOL environment variable'
+	echo >&2 '  Did you forget to -e PROTOCOL=... ?'
+	exit 1
+fi
+
+
+
+
+
+else
+	echo >&2 'error: missing required TOKEN environment variable'
+	echo >&2 '  Did you forget to -e TOKEN=... ?'
+	exit 1
+fi
+else
+	echo >&2 'error: missing required SERVER_PORT environment variable'
+	echo >&2 '  Did you forget to -e SERVER_PORT=... ?'
+	exit 1
+fi
+else
+	echo >&2 'error: missing required SERVER_ADDR environment variable'
+	echo >&2 '  Did you forget to -e SERVER_ADDR=... ?'
+	exit 1
+fi
+
+
+
+
+
+
+
+
+
+
+
+exit 0
+
+
+
+
+
+
+
+
+
